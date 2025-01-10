7a3a9f6f2d5c8cd1fe43b4d0322bb1f902515b9c
Victoria Fonteny <victoriafonteny@live.fr>
2025-01-10 12:12:34 +0100

    show question - fix page d'erreur

--- client/src/pages/Quizz/Quizz.tsx
+++ client/src/pages/Quizz/Quizz.tsx
@@ -1,9 +1,60 @@
-import ShowQuestion from "../../components/ShowQuestion";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface CountriesType {
+  name: {
+    common: string;
+  };
+  flags: {
+    png: string;
+  };
+}
+
 export default function Quizz() {
+  const [countries, setCountries] = useState([] as CountriesType[]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then((response) => {
+        setCountries(response.data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+        setIsLoading(false);
+      });
+  }, []);
+
+  const nbsRandom: number[] = [];
+
+  if (countries) {
+    while (nbsRandom.length < 3) {
+      const random = Math.floor(Math.random() * countries.length);
+      if (!nbsRandom.includes(random)) {
+        nbsRandom.push(random);
+      }
+    }
+  }
+
+  const goodAnswerIndex = Math.floor(Math.random() * nbsRandom.length - 1);
+  const goodAnswer = nbsRandom[goodAnswerIndex];
+
+  if (isLoading || !goodAnswer) {
+    return <h3>loading refresh</h3>;
+  }
+
   return (
     <>
-      <h2>Thème</h2> {/* Recuper en props le thème choisit */}
-      <ShowQuestion />
+      <h2>Thème</h2>
+      <h3> Quel est le drapeau de {countries[goodAnswer].name.common} ?</h3>
+      <p>good answer : </p>
+      <img src={countries[goodAnswer].flags.png} alt="" />
+      <p>réponses possibles : </p>
+      <img src={countries[nbsRandom[0]].flags.png} alt="" />
+      <img src={countries[nbsRandom[1]].flags.png} alt="" />
+      <img src={countries[nbsRandom[2]].flags.png} alt="" />
     </>
   );
 }
