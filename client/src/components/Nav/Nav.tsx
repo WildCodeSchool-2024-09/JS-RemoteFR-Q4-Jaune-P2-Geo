ca5fc58ddefbf6e6eda0f2c43cd76e701c029148
Killian0111 <45.killian.georges@gmail.com>
2025-01-10 10:24:07 +0100

    nav

--- client/src/components/Nav/Nav.tsx
+++ client/src/components/Nav/Nav.tsx
@@ -1,7 +1,23 @@
+import { Link } from "react-router-dom";
+
 export default function Nav() {
   return (
-    <>
-      <h1>Nav</h1>
-    </>
+    <nav>
+      <img
+        src="/public/images/avion2.png"
+        alt="C'est un logo qui represente un avion qui vole."
+      />
+      <ul>
+        <li>
+          <Link to="/">Accueil</Link>
+        </li>
+        <li>
+          <Link to="/Rules">Règles</Link>
+        </li>
+        <li>
+          <Link to="/Contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
