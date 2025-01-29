78df57ccdca5b7f4843849059336b9e35b07fa49
Victoria Fonteny <victoriafonteny@live.fr>
2025-01-29 17:03:40 +0100

    use hook for external logique

--- client/src/pages/Capitals/Capitals.tsx
+++ client/src/pages/Capitals/Capitals.tsx
@@ -1,128 +1,38 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import Result from "../../components/Result";
+import useQuestion from "../../services/hooks/useQuestion";
+import useTimer from "../../services/hooks/useTimer";
 
 export default function Capitals({ countries }: ThemeProps) {
-  const [nbsRandom, setNbsRandom] = useState([] as number[]);
-  const [goodAnswer, setGoodAnswer] = useState(0 as number);
-  const [questionCount, setQuestionCount] = useState(0);
-  const [userChoiceIndex, setUserChoiceIndex] = useState(0 as number);
-  const [dialogOpen, setDialogOpen] = useState(false);
-  const [isValidate, setIsValidate] = useState(false);
-  const [score, setScore] = useState(0);
   const [isAnswerSelected, setIsAnswerSelected] = useState(false);
-  const [timer, setTimer] = useState(10);
-  const [timerColor, setTimerColor] = useState("green");
+  const [message, setMessage] = useState("");
 
-  //Timer (avec setInterval)
-  useEffect(() => {
-    if (timer > 0 && !isAnswerSelected) {
-      const interval = setInterval(() => {
-        setTimer((prevTimer) => {
-          const newTimer = prevTimer - 1;
-          if (newTimer <= 6) {
-            setTimerColor("orange");
-          }
-          if (newTimer <= 3) {
-            setTimerColor("red");
-          }
-          return newTimer;
-        });
-      }, 1000);
-      return () => clearInterval(interval);
-    }
+  const { timer, timerColor, setTimer, setTimerColor } =
+    useTimer(isAnswerSelected);
 
-    if (timer === 0) {
-      setDialogOpen(true);
-      setIsValidate(false);
-      setIsAnswerSelected(true);
-      return;
-    }
-  }, [timer, isAnswerSelected]);
-
-  // Fonction pour générer des indices de pays aléatoires UNIQUE (condition if)
-  const generateAnswer = () => {
-    const arrayNbs = [] as number[];
-    while (arrayNbs.length < 3) {
-      const num = Math.floor(Math.random() * countries.length);
-      if (!arrayNbs.includes(num)) {
-        arrayNbs.push(num);
-      }
-    }
-    return arrayNbs;
-  };
-
-  // Fonction pour générer une nouvelle question avec de nouvelles réponses aléatoires
-  const handleNextQuestion = () => {
-    const randomAnswers = generateAnswer();
-    const correctAnswer =
-      randomAnswers[Math.floor(Math.random() * randomAnswers.length)];
-
-    setNbsRandom(randomAnswers); // Met à jour les réponses possibles
-    setGoodAnswer(correctAnswer); // Met à jour la bonne réponse
-    setQuestionCount((prevCount) => prevCount + 1);
-    closeDialog();
-    setIsAnswerSelected(false);
-    setTimer(10);
-    setTimerColor("green");
-  };
-  console.info(countries);
-
-  const handleChoiseAnswer = (countryIndex: number) => {
-    if (isAnswerSelected) return;
-    setIsAnswerSelected(true);
-
-    setUserChoiceIndex(countryIndex);
-    setDialogOpen(true);
-
-    if (countryIndex === goodAnswer) {
-      setIsValidate(true);
-    } else {
-      setIsValidate(false);
-    }
-
-    if (
-      countries[countryIndex].name.common === countries[goodAnswer].name.common
-    ) {
-      setScore((prevScore) => prevScore + 1);
-    }
-  };
-
-  const closeDialog = () => {
-    setDialogOpen(false);
-  };
-
-  // Si les pays sont chargés, générer la première question
-  if (countries.length && nbsRandom.length === 0) {
-    handleNextQuestion();
-  }
+  const {
+    score,
+    questionCount,
+    userChoiceIndex,
+    goodAnswer,
+    nbsRandom,
+    handleChoiceAnswer,
+    handleNextQuestion,
+    dialogOpen,
+    isValidate,
+  } = useQuestion(
+    countries,
+    setIsAnswerSelected,
+    isAnswerSelected,
+    setTimer,
+    setTimerColor,
+    timer,
+  );
 
   if (questionCount === 11) {
-    if (score === 10) {
-      return <h2>{score}/10 - Félicitations un vrai globe-trotters !</h2>;
-    }
-    if (score >= 8) {
-      return <h2>{score}/10 - Un petit effort et tu seras au top !</h2>;
-    }
-    if (score >= 6) {
-      return <h2>{score}/10 - Continue à explorer !</h2>;
-    }
-    if (score >= 4) {
-      return (
-        <h2>
-          {score}/10 - Tu es sur la bonne voie mais il reste encore beaucoup à
-          découvrir !
-        </h2>
-      );
-    }
-    if (score >= 2) {
-      return (
-        <h2>
-          {score}/10 - Ce n'est qu'un début, mais il te reste encore du chemin à
-          parcourir !
-        </h2>
-      );
-    }
-    return <h2>{score}/10 - Tu as encore du chemin à faire !</h2>;
+    return <Result score={score} message={message} setMessage={setMessage} />;
   }
+
   return (
     <>
       <div className="timer" style={{ color: timerColor }}>
@@ -148,7 +58,7 @@ export default function Capitals({ countries }: ThemeProps) {
             <button
               key={index}
               type="button"
-              onClick={() => handleChoiseAnswer(index)}
+              onClick={() => handleChoiceAnswer(index)}
             >
               {countries[index].capital}
             </button>
@@ -157,7 +67,7 @@ export default function Capitals({ countries }: ThemeProps) {
 
         <dialog className={isValidate ? "good" : "notGood"} open={dialogOpen}>
           {timer > 0 && (
-            <p> Réponse choisit : {countries[userChoiceIndex].capital} </p>
+            <p> Réponse choisit : {countries[userChoiceIndex].capital[0]} </p>
           )}
           {timer === 0 && <p>Temps écoulés !</p>}
           <p>
@@ -166,7 +76,7 @@ export default function Capitals({ countries }: ThemeProps) {
               ? "Bien joué ! C'était bien :"
               : "Dommage, la réponse était :"}
           </p>
-          <p className="goodAnswer">{countries[goodAnswer].capital}</p>
+          <p className="goodAnswer">{countries[goodAnswer].capital[0]}</p>
           <button type="button" onClick={handleNextQuestion}>
             Question suivante
           </button>
